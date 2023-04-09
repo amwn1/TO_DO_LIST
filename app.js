b1666cb9c3bff04d4b5c939a533a49f8d9440f78
Aman <amanmalik3105@gmail.com>
2023-04-09 20:14:28 +0530

    playing around with some dependecies

--- app.js
+++ app.js
@@ -2,11 +2,42 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+app.set('view engine', 'ejs'); // always place it below const app.
+app.get("/", (req, res) => {
+    var today = new Date();
+    var currentDay = today.getDay();
+    var day = "";
 
-app.get("/",(req,res)=>{
-     res.sendFile(__dirname+"/index.html");
+    switch (currentDay) {
+        case 0:
+            day = "Sunday";
+            break;
+        case 1:
+            day = "Monday";
+            break;
+        case 2:
+            day = "Tuesday";
+            break;
+        case 3:
+            day = "Wednesday";
+            break;
+        case 4:
+            day = "Thursday";
+            break;
+        case 5:
+            day = "Friday";
+            break;
+        case 6:
+            day = "Saturday";
+            break;
+        default:
+            console.log("Error here")
+    }
+
+    res.render("list", { kindofDay: day });
+
 })
 
-app.listen(3000, () =>{
+app.listen(3000, () => {
     console.log("Server is running at port 3000");
 })
