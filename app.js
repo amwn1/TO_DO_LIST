e409a1fa40f2654a176671c0e36676ce06bae74b
Aman <amanmalik3105@gmail.com>
2023-04-19 19:23:19 +0530

    Header and footer Layout for an about page

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 let items =["Buy food " , " eat","some some example"];
+let workItems = [];
 app.set('view engine', 'ejs'); // always place it below const app.
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
@@ -18,17 +19,34 @@ app.get("/", (req, res) => {
    
     let day = today.toLocaleDateString("en-US",options)
   
-    res.render("list", { kindofDay: day ,newListItems: items});
+    res.render("list", { listTitle: day ,newListItems: items});
 
 })
 
 app.post("/",(req,res) => {
+    console.log(req.body);
    let item = req.body.newItem;
     items.push(item);
     res.redirect("/");
+  
+})
 
+app.get("/work",(req,res) => {
+    res.render("list",{listTitle: "Work List" , newListItems: workItems});
+})
+
+app.get("/about",(req,res)=>{
+    res.render("about")
+})
+
+app.post("/work",(req,res) => {
+    let item = req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work");
 })
 
 app.listen(3000, () => {
     console.log("Server is running at port 3000");
 })
+
+
